290f36eadc89f9027968d53cdafa68accf68769e
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2025-04-15 19:51:28 +0330

    :art: improve: Post, Rating API

--- apps/blog/api/v1/serializers/post_serializer.py
+++ apps/blog/api/v1/serializers/post_serializer.py
@@ -1,21 +1,30 @@
 from rest_framework import serializers
 
-from apps.blog.models.post_model import Post
-
 from taggit.serializers import TagListSerializerField
+
+from apps.blog.models.post_model import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
 
+    # initial fields
     author = serializers.SerializerMethodField()
     tags = TagListSerializerField()
+
+    # rating serializer
+    rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = [
             "id", "author", "cover", "title", "slug", "body", 
-            "status","tags", "created_at", "updated_at"
+            "status","tags", "rating", "created_at", "updated_at"
         ]
 
     def get_author(self, post: Post):
         return post.author.email
+    
+    def get_rating(self, post: Post):
+        from apps.blog.api.v1.serializers.rating_serializer import RatingSerializer
+        rating = post.post_rating.select_related("user", "post").all()
+        return RatingSerializer(rating, many=True).data
